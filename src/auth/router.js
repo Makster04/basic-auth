406f8f8da1f4aa2bbd5d13be26aa6a0165e1cc01
MAK TRNKA <maktrnka@comcast.net>
2024-04-08 23:44:01 -0700

    All updated

---
+++ src/auth/router.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require("express");
+const { basicAuth, signUp } = require("./middleware/basic.js");
+
+const router = express.Router();
+
+router.post("/signup", signUp, async (req, res) => {
+  res.status(200).send("You signed up!!!");
+});
+
+router.post("/signin", basicAuth, async (req, res) => {
+  res.status(200).send("Welcome to the app!!!");
+});
+
+module.exports = router;
